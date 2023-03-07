4bb383f6a0c8732351ff7affaaeee8f608a58fd9
Károly Szakály <karoly.szakaly2000@gmail.com>
2023-03-07 11:13:22 +0100

    feat(webapp): add basic Express app

--- webapp/index.js
+++ webapp/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ status: "ok" });
+});
+
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
