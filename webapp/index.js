b1ed33bdc89eed2bd1c0308012f9b78a31d6935a
Károly Szakály <karoly.szakaly2000@gmail.com>
2023-03-08 20:59:43 +0100

    feat(webapp): add endpoint to query users

--- webapp/index.js
+++ webapp/index.js
@@ -1,15 +1,26 @@
 import express from "express";
 
-import usersJson from "./constants/users.json" assert { type: "json" };
+import users from "./constants/users.json" assert { type: "json" };
 
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ status: "ok" });
+  return res.status(200).json({ status: "success" });
 });
 
 app.get("/users", (req, res) => {
-  return res.status(200).json({ status: "ok", users: usersJson });
+  return res.status(200).json({ status: "success", users: users });
+});
+
+app.get("/users/:username", (req, res) => {
+  const { username } = req.params;
+
+  const requestedUser = users.find((user) => user.username == username);
+  if (!requestedUser) {
+    return res.status(404).json({ status: "error", reason: `Couldn't find user with username ${username}` });
+  }
+
+  return res.status(200).json({ status: "success", user: requestedUser });
 });
 
 const port = process.env.PORT ?? 3000;
