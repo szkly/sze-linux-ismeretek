18ebaad4519a6d01100e352c2bd6243d9d3a6b5e
Károly Szakály <karoly.szakaly2000@gmail.com>
2023-03-08 20:52:35 +0100

    feat(webapp): add users endpoint

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 
+import usersJson from "./constants/users.json" assert { type: "json" };
+
 const app = express();
 
 app.get("/", (req, res) => {
   return res.status(200).json({ status: "ok" });
+});
+
+app.get("/users", (req, res) => {
+  return res.status(200).json({ status: "ok", users: usersJson });
 });
 
 const port = process.env.PORT ?? 3000;
