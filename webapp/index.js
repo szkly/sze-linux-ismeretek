7e74ad03aef61d376bc890d38b7a6f29d2419485
Károly Szakály <karoly.szakaly2000@gmail.com>
2023-03-27 21:47:29 +0200

    feat(webapp): implement dummy Node.js app (#1)
    * feat(webapp): add basic Express app
    
    * feat(webapp): add users endpoint
    
    * feat(webapp): add endpoint to query users
    
    * feat(docker): containerize app
    
    * fix(docker): add missing EXPOSE instruction

--- webapp/index.js
+++ webapp/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+import express from "express";
+
+import users from "./constants/users.json" assert { type: "json" };
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ status: "success" });
+});
+
+app.get("/users", (req, res) => {
+  return res.status(200).json({ status: "success", users: users });
+});
+
+app.get("/users/:username", (req, res) => {
+  const { username } = req.params;
+
+  const requestedUser = users.find((user) => user.username == username);
+  if (!requestedUser) {
+    return res.status(404).json({ status: "error", reason: `Couldn't find user with username ${username}` });
+  }
+
+  return res.status(200).json({ status: "success", user: requestedUser });
+});
+
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
